20e42ff81411299284a1526a70e183a0a5c37282
CristhianRF24 <201803525@est.umss.edu>
2024-09-30 10:53:19 -0400

    Fix mysql and docker connection

--- app/db/db.py
+++ app/db/db.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root@localhost:3306/enterprice_chat'
-# URL_DATABASE = 'mysql+pymysql://root:12345@mysql-container:3306/enterprice_chat'
+URL_DATABASE = 'mysql+mysqlconnector://root:root@db:3306/enterprise_chat'
 
 
 
