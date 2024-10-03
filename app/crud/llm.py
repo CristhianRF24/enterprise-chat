7774f08dec310b44aad79bbc7b98c576d7b7fbc3
CristhianRF24 <201803525@est.umss.edu>
2024-10-03 17:19:45 -0400

    integrate openai

--- app/crud/llm.py
+++ app/crud/llm.py
@@ -1,13 +1,13 @@
 
-import os
+from fastapi import HTTPException
 import openai
 from requests import Session
 from app.api.v1.endpoints.files import get_database_schema
+import json
 
 
 
-def generate_sql_query(user_query: str, db: Session):
-   #get database schema
+def generate_sql_query(user_query: str, db: Session) -> str:
     schema = get_database_schema(db)
     
     system_message = f"""
@@ -15,9 +15,10 @@ def generate_sql_query(user_query: str, db: Session):
     Return the SQL query inside a JSON structure with the key "sql_query".
     <example>
     {{
-        "sql_query": "SELECT * FROM files;"
+        "sql_query": "SELECT * FROM files",
         "original_query": "Show me all the files."
     }}
+    
     </example>
     <schema>
     {schema}
@@ -25,11 +26,18 @@ def generate_sql_query(user_query: str, db: Session):
     """
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {"role": "system", "content": system_message},
             {"role": "user", "content": user_query}
         ]
     )
+
+    response_content = response.choices[0].message["content"]
     
-    return response.choices[0].message.content
+    try:
+        response_json = json.loads(response_content)  
+        sql_query = response_json.get("sql_query") 
+        return sql_query
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=500, detail="Error parsing the AI response.")
