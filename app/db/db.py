3952c4a26080116c7b133efd4936855f278710ac
CristhianRF24 <201803525@est.umss.edu>
2024-10-08 17:02:34 -0400

    .env

--- app/db/db.py
+++ app/db/db.py
@@ -8,6 +8,13 @@ from typing import List, Dict
 
 load_dotenv() 
 
+db_config = {
+    "host": os.getenv('DB_HOST'), 
+    "user": os.getenv('DB_USER'),       
+    "password":  os.getenv('DB_PASS'),      
+    "database":  os.getenv('DB_NAME'), 
+    "port": os.getenv('DB_PORT')        
+}
 
 URL_DATABASE = os.getenv('DATABASE_URL')
 
