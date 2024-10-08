f055c80447748e11f7250bc419558d289e1c58f8
Belen <beckiar23@gmail.com>
2024-10-08 18:45:56 -0400

    Sparql query generation with an LLM has been implemented

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 from requests import Session
 from app.api.v1.endpoints import files
-from app.crud.llm import generate_sql_query
+from app.crud.llm import generate_sparql_query, generate_sql_query
 from app.db.db import execute_sql_query, get_db, is_sql_query_safe
+from rdflib import Graph
 
 app = FastAPI()
 
@@ -12,6 +13,14 @@ class SQLQueryRequest(BaseModel):
     model: str
 class SQLQueryResponse(BaseModel): 
     results: list
+    
+class QueryRequest(BaseModel):
+    user_query: str
+    model: str
+
+class QueryResponse(BaseModel):
+    query: str 
+    results: dict 
 
 app.include_router(files.router, prefix="/files", tags=["files"])
 
@@ -30,3 +39,21 @@ async def generate_sql(request: SQLQueryRequest, db: Session = Depends(get_db)):
   
     return SQLQueryResponse(results=results)
     
+def load_graph():
+    knowledge_graph = Graph()
+    knowledge_graph.parse("output.ttl", format="turtle")  
+    return knowledge_graph
+
+@app.post("/generate_sparql/", response_model=QueryResponse)
+async def generate_sparql(request: QueryRequest, db: Session = Depends(get_db)):
+    sparql_query = generate_sparql_query(request.user_query, db, model=request.model)
+    print('sparql_query', sparql_query)
+    
+    knowledge_graph = load_graph() 
+    results = knowledge_graph.query(sparql_query)
+    results_dict = {}
+    for row in results:
+        for var in results.vars:
+            results_dict[str(var)] = str(row[var])  
+
+    return QueryResponse(query=sparql_query, results=results_dict)
